cd1c52f50e92ecd37ae179f0e55b533c33f8062b
Anton Antich <anton.antich@gmail.com>
2020-12-10 23:09:59 +0100

    reused some wrapper functions from sdl-examples

--- examples/ng/NewGenTest.hs
+++ examples/ng/NewGenTest.hs
@@ -11,13 +11,58 @@ import PreludeFixes
 import Control.Exception ( try )
 import Data.Either
 
+import Control.Monad.Extra(whileM)
+
 import SDL
 import SDL.Font
 
 import UI.Femto.SDL.Renderable
 
+import UI.Femto.SDL.Common as C
+
+oldmain :: IO ()
+oldmain = initStateIO
+
+
 main :: IO ()
-main = initStateIO
+main = C.withSDL $ do
+  C.setHintQuality
+  C.withWindow "Test" (1200, 800) $ \w ->
+    C.withRenderer w $ \ren -> do
+
+      mfont <- SDL.Font.initialize >> unsafeLoadFont defaultFontPath 32
+      print "Loaded font"
+      let st = RWSimpleTextLine { -- text line with different styles but the same font and size
+                  font = mfont,
+                  text = testText,
+                  cursorPos = 0
+                }
+      let testEl = mkRWSimpleTextLine st "id0"
+      let testEl' = renderElement ren testEl
+      tex <- render ren st
+      dest <- rectangleFromTexture tex 50 50
+      -- let (Just tex'') = texCache testEl'
+      tex' <- texCache testEl'
+      dest' <- rectangleFromTexture tex' 50 150
+      rendererDrawColor ren $= mWhite
+      
+
+      whileM $
+        C.isContinue <$> SDL.pollEvent
+        >>= C.conditionallyRun (draw ren (tex,dest) (tex', dest') )
+
+      destroyTexture tex
+      destroyTexture tex'
+      free mfont
+
+
+-- draw :: SDL.Renderer -> SDL.Texture -> IO ()
+draw r (t,d) (t1,d1) = do
+  SDL.clear r
+  SDL.copy r t Nothing (Just d)
+  SDL.copy r t1 Nothing (Just d1) 
+  SDL.present r
+
 
 testText = SDLStyledText {
     text = "Hellow New Generation AGAIN!",
@@ -36,6 +81,7 @@ initStateIO = do
             print "SDL Initialized (hopefully)"
             window <- SDL.createWindow "My SDL Application" mainWindowSettings
             print $ "SDL Window created:\n" ++ show window 
+            SDL.showWindow window
             ren <- SDL.createRenderer window (-1) SDL.defaultRenderer {SDL.rendererTargetTexture = True}
             print "SDL Renderer initialized"
             mfont <- SDL.Font.initialize >> unsafeLoadFont defaultFontPath 32
@@ -59,7 +105,7 @@ initStateIO = do
             present ren
             destroyTexture tex
             free mfont
-            SDL.delay 5000
+            SDL.delay 50000
     either (\e -> print (e::SDLException) >> fail "Could not initialize SDL")
            (\st -> putStrLn "Initialized SDL") r
 
